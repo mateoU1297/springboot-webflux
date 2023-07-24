7bf9ef3edeafb0558df31024e05bd3a842996d5a
mateoU1297 <castro.mateo@correounivalle.edu.co>
2023-07-24 12:33:28 -0500

    se agrega repositorio

---
+++ src/test/java/com/udemy/springboot/webflux/app/SpringbootWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.springboot.webflux.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
